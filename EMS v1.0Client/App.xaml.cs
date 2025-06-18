422bc5b344127114649ac43924cbfa02e548e0e8
D1ngirsu <dingirsu2800@gmail.com>
2025-06-18 11:16:19 +0700

    Add Client

---
+++ EMS v1.0Client/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EMS_v1._0Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
